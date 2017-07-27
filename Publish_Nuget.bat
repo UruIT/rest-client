27698b03119e1353fc40c98379f8943ab5127113
CARRASCO\gwaszczuk <gonzalo.waszczuk@uruit.com>
2017-07-27 11:19:41 -0300

    Make final updates before 1.0 release
    - Makes the sample reference the DLLs directly (referencing the VS projects inside its solution) instead of referencing the NuGet package
    - Updates the NuGet version to v1.0.0

--- Publish_Nuget.bat
+++ Publish_Nuget.bat
@@ -1,5 +1,5 @@
 REM Publishes de package to Nuget.org
 
-nuget push UruIT.RESTClient.1.0.0-alpha-1-1.nupkg -Source https://www.nuget.org/api/v2/package
+nuget push UruIT.RESTClient.1.0.0.nupkg -Source https://www.nuget.org/api/v2/package
 
 pause
